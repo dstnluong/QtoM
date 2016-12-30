8d4e298e320bf68773c42ad52d36511f922741ef
Dustin Luong <dstn.luong@gmail.com>
2016-12-29 19:03:40 -0800

    Converted to Q to M

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@
     if (Cookies.get("cookieUserConsent") !== "true") {
       $("#cookies").css("visibility", "visible");
     }
-    return $("#a, #b, #c, #d, #e, #f, #g, #h, #i, #j, #k, #l, #m, #n, #o, #p, #q, #r, #s, #t, #u, #v, #w, #x, #y, #z").click(function() {
+    return $("#q, #w, #e, #r, #t, #y, #u, #i, #o, #p, #a, #s, #d, #f, #g, #h, #j, #k, #l, #z, #x, #c, #v, #b, #n, #m").click(function() {
       return chooseStart(this.innerHTML);
     });
   });
